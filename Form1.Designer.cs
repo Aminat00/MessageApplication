08c30ff8cd0c617f4629bf615d52e7ef203503dc
Aminat00 <38074109+Aminat00@users.noreply.github.com>
2022-01-26 07:41:16 +0100

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,330 @@
+﻿
+namespace MessageApplication
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.listBox2 = new System.Windows.Forms.ListBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(400, 151);
+            this.richTextBox1.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(320, 130);
+            this.richTextBox1.TabIndex = 1;
+            this.richTextBox1.Text = "";
+            this.richTextBox1.TextChanged += new System.EventHandler(this.richTextBox1_TextChanged);
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.PaleVioletRed;
+            this.button1.Location = new System.Drawing.Point(630, 401);
+            this.button1.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(136, 40);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Broadcast";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.Location = new System.Drawing.Point(0, 0);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(112, 21);
+            this.label1.TabIndex = 5;
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 18;
+            this.listBox1.Location = new System.Drawing.Point(799, 88);
+            this.listBox1.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(320, 130);
+            this.listBox1.TabIndex = 4;
+            this.listBox1.SelectedIndexChanged += new System.EventHandler(this.listBox1_SelectedIndexChanged);
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.Color.PaleVioletRed;
+            this.button2.Location = new System.Drawing.Point(882, 224);
+            this.button2.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(164, 40);
+            this.button2.TabIndex = 6;
+            this.button2.Text = "Show";
+            this.button2.UseVisualStyleBackColor = false;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(370, 372);
+            this.textBox1.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(200, 26);
+            this.textBox1.TabIndex = 7;
+            // 
+            // button3
+            // 
+            this.button3.BackColor = System.Drawing.Color.PaleVioletRed;
+            this.button3.Font = new System.Drawing.Font("Century Gothic", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.button3.Location = new System.Drawing.Point(23, 261);
+            this.button3.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(145, 40);
+            this.button3.TabIndex = 8;
+            this.button3.Text = "Add user";
+            this.button3.UseVisualStyleBackColor = false;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.BackColor = System.Drawing.Color.PaleVioletRed;
+            this.button4.Font = new System.Drawing.Font("Century Gothic", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.button4.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.button4.Location = new System.Drawing.Point(208, 261);
+            this.button4.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(124, 40);
+            this.button4.TabIndex = 9;
+            this.button4.Text = "Delete user";
+            this.button4.UseVisualStyleBackColor = false;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // button5
+            // 
+            this.button5.BackColor = System.Drawing.Color.PaleVioletRed;
+            this.button5.Location = new System.Drawing.Point(412, 404);
+            this.button5.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(136, 37);
+            this.button5.TabIndex = 10;
+            this.button5.Text = "Direct";
+            this.button5.UseVisualStyleBackColor = false;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Century Gothic", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label2.ForeColor = System.Drawing.Color.Maroon;
+            this.label2.Location = new System.Drawing.Point(356, 9);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(473, 34);
+            this.label2.TabIndex = 11;
+            this.label2.Text = "Welcome to Message Application";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Century Gothic", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label3.ForeColor = System.Drawing.Color.Maroon;
+            this.label3.Location = new System.Drawing.Point(356, 300);
+            this.label3.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(221, 57);
+            this.label3.TabIndex = 12;
+            this.label3.Text = "Write the name of the user\r\n to whom you want to\r\n send message";
+            this.label3.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            this.label3.UseMnemonic = false;
+            // 
+            // listBox2
+            // 
+            this.listBox2.FormattingEnabled = true;
+            this.listBox2.ItemHeight = 18;
+            this.listBox2.Location = new System.Drawing.Point(799, 322);
+            this.listBox2.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            this.listBox2.Name = "listBox2";
+            this.listBox2.Size = new System.Drawing.Size(320, 130);
+            this.listBox2.TabIndex = 13;
+            this.listBox2.SelectedIndexChanged += new System.EventHandler(this.listBox2_SelectedIndexChanged);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(113, 208);
+            this.textBox2.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(140, 26);
+            this.textBox2.TabIndex = 14;
+            this.textBox2.TextChanged += new System.EventHandler(this.textBox2_TextChanged);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Century Gothic", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label4.ForeColor = System.Drawing.Color.Maroon;
+            this.label4.Location = new System.Drawing.Point(82, 132);
+            this.label4.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(195, 57);
+            this.label4.TabIndex = 15;
+            this.label4.Text = "We kindly request you \r\nto add user before \r\nsending message";
+            this.label4.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Century Gothic", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label5.ForeColor = System.Drawing.Color.Maroon;
+            this.label5.Location = new System.Drawing.Point(443, 117);
+            this.label5.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(226, 19);
+            this.label5.TabIndex = 16;
+            this.label5.Text = "Write your message below";
+            this.label5.Click += new System.EventHandler(this.label5_Click);
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.BackColor = System.Drawing.Color.DarkTurquoise;
+            this.label6.Font = new System.Drawing.Font("Century Gothic", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label6.ForeColor = System.Drawing.Color.Maroon;
+            this.label6.Location = new System.Drawing.Point(617, 301);
+            this.label6.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(149, 76);
+            this.label6.TabIndex = 17;
+            this.label6.Text = "No need to write \r\nthe user name\r\n it will broadcast \r\nthe message";
+            this.label6.Click += new System.EventHandler(this.label6_Click);
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Century Gothic", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label7.ForeColor = System.Drawing.Color.Maroon;
+            this.label7.Location = new System.Drawing.Point(862, 273);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(214, 36);
+            this.label7.TabIndex = 18;
+            this.label7.Text = "Messages from both \r\ndirect and fanout exchange";
+            this.label7.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            this.label7.Click += new System.EventHandler(this.label7_Click);
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(829, 59);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(247, 18);
+            this.label8.TabIndex = 19;
+            this.label8.Text = "Messages from fanout exchange";
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.BackColor = System.Drawing.Color.SkyBlue;
+            this.label9.Location = new System.Drawing.Point(440, 59);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(229, 54);
+            this.label9.TabIndex = 20;
+            this.label9.Text = "Please write your first message\r\n to specific user then use\r\n boradcasting messag" +
+    "e";
+            this.label9.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 18F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.LightSkyBlue;
+            this.ClientSize = new System.Drawing.Size(1268, 480);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.listBox2);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.richTextBox1);
+            this.Font = new System.Drawing.Font("Century Gothic", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.ForeColor = System.Drawing.Color.Maroon;
+            this.Name = "Form1";
+            this.Text = "Messages from direct exchange";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.ListBox listBox2;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label9;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,203 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using RabbitMQ.Client;
+using System.Configuration;
+using RabbitMQ.Client.Events;
+
+namespace MessageApplication
+{
+    
+
+    public partial class Form1 : Form
+    {
+        private IModel channel_message;
+        private IConnection connection;
+
+        public Form1()
+        {
+            InitializeComponent();
+            
+            var ConnectionString = ConfigurationManager.ConnectionStrings["Connection_name"].ConnectionString;
+
+            var ConnectionFactory = new ConnectionFactory() { };
+            ConnectionFactory.Uri = new Uri(ConnectionString);
+            ConnectionFactory.AutomaticRecoveryEnabled = true;
+            ConnectionFactory.DispatchConsumersAsync = true;
+            connection = ConnectionFactory.CreateConnection("chatting");
+        }
+      
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+            channel_message = connection.CreateModel();
+            {
+                channel_message.ExchangeDeclare(exchange: "pubsub", type: ExchangeType.Fanout);
+                string message = richTextBox1.Text;
+                    var body = Encoding.UTF8.GetBytes(message);
+                    channel_message.BasicPublish(exchange: "pubsub",
+                    routingKey: "",
+                    basicProperties: null,
+                    body: body);
+                    MessageBox.Show("Message sent successfully.");
+             }
+            
+        }
+
+        private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+ 
+        }
+
+        private async Task channel_messageConsumer_Received(object sender, BasicDeliverEventArgs e)
+        {
+            
+           
+
+            if (String.IsNullOrEmpty(textBox1.Text))
+            {
+                channel_message = connection.CreateModel();
+                channel_message.BasicAck(e.DeliveryTag, false);
+
+                var msg = Encoding.UTF8.GetString(e.Body.ToArray());
+                listBox1.Invoke((MethodInvoker)(() => listBox1.Items.Add(msg)));
+                var message1 = Encoding.UTF8.GetString(e.Body.ToArray());
+                listBox2.Invoke((MethodInvoker)(() => listBox2.Items.Add(message1)));
+               
+            }
+            else
+            {
+                channel_message = connection.CreateModel();
+                channel_message.BasicAck(e.DeliveryTag, false);
+                var message = Encoding.UTF8.GetString(e.Body.ToArray());
+                listBox2.Invoke((MethodInvoker)(() => listBox2.Items.Add(message)));
+            }
+            
+
+
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+
+            channel_message = connection.CreateModel();
+            channel_message.BasicQos(0, 1, false);
+            var channel_messageConsumer = new AsyncEventingBasicConsumer(channel_message);
+            channel_messageConsumer.Received += channel_messageConsumer_Received;
+
+            if (String.IsNullOrEmpty(textBox1.Text))
+            {
+
+                var queueName = channel_message.QueueDeclare().QueueName;
+                channel_message.QueueBind(queue: queueName,
+                                  exchange: "pubsub",
+                                  routingKey: "");
+                channel_message.BasicConsume(queueName, true, channel_messageConsumer);
+            }
+            else
+            {
+
+                var user = textBox1.Text;
+
+                channel_message.BasicConsume(user, true, channel_messageConsumer);
+
+
+            }
+        }
+
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            using (var channel = connection.CreateModel())
+            {
+                var user = textBox2.Text;
+                channel.QueueDeclare(user, true, false, false);
+                channel.QueueBind(user, "pubsub", user);
+                channel.QueueBind(user, "direct", user);
+
+            }
+            MessageBox.Show("User added successfully.");
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            using (var channel = connection.CreateModel())
+            {
+                var user = textBox2.Text;
+                channel.QueueDelete(user);
+            }
+            MessageBox.Show("User deleted successfully.");
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            using (var channel = connection.CreateModel())
+            {
+                var user = textBox1.Text;
+                try
+                {
+                    channel.ExchangeDeclare(exchange: "direct", type: ExchangeType.Direct);
+                    channel.QueueBind(user, "direct", "useronly");
+
+                    string message = richTextBox1.Text;
+                    var body = Encoding.UTF8.GetBytes(message);
+                    channel.BasicPublish(exchange: "direct",
+                        user,
+                        basicProperties: null,
+                        body: body);
+                    MessageBox.Show("Message sent successfully.");
+                }
+                catch (Exception)
+                {
+
+                    MessageBox.Show("User is not added in the group!");
+                }
+                
+            }
+          
+        }
+
+        private void richTextBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label5_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void listBox2_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label6_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label7_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}    
